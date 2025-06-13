2de830bb6134503a8d93055c0dd0859e520369e5
ao9 <ao9@iu.edu>
2025-06-13 11:25:12 -0500

    First commit

---
+++ back/server.js
@@ -0,0 +1,15 @@
+// Entry to backend/api
+// not using common js instead using es modules
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 8080;// cant use port 5000 like video because of ControlCe
+
+const app = express();
+
+app.get('/', (req,res) => res.send('Server is ready'));
+
+app.listen(port,() => console.log(`Server started on port: ${port}`));
