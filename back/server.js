9d37258cbc32b6c657341a866932d21b529f3a36
ao9 <ao9@iu.edu>
2025-06-15 10:04:46 -0500

    Database setup, 44:55

--- back/server.js
+++ back/server.js
@@ -2,14 +2,16 @@
 // not using common js instead using es modules
 
 import express from 'express';
-import dotenv from 'dotenv';
+import dotenv, { config } from 'dotenv';
 import userRoutes from './routes/user-routes.js';
 import { notFound,errorHandler } from './middleware/error-middleware.js';
+import connectDB from './config/db.js';
  
+dotenv.config();
+
+connectDB();
 
 const app = express();
-
-dotenv.config();
 
 const port = process.env.PORT || 8080;// cant use port 5000 like video because of ControlCe
 
