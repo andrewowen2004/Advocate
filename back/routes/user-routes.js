f41017f79f5e997464d678ee4984a499668bac72
ao9 <ao9@iu.edu>
2025-06-15 19:51:33 -0500

    routes UTD

--- back/routes/user-routes.js
+++ back/routes/user-routes.js
@@ -1,15 +1,21 @@
-
-
 import express from 'express';
-import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile } from '../controllers/user-controller.js';
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/user-controller.js';
+import { protect } from '../middleware/auth-middleware.js';
 
 const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile')
-  .get(getUserProfile)
-  .put(updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
