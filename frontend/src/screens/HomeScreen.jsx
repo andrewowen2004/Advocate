b4f8a0693e32e04eae236fb8758165433acd0927
ao9 <ao9@iu.edu>
2025-06-16 15:46:42 -0500

    Removed Hero.jsx from home screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,14 @@
-import Hero from '../components/Hero';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const HomeScreen = () => {
-  return <Hero />;
+  return (
+    <Container className="py-5">
+      <Row className="justify-content-center">
+        <Col md={8} className="text-center">
+        </Col>
+      </Row>
+    </Container>
+  );
 };
+
 export default HomeScreen;
